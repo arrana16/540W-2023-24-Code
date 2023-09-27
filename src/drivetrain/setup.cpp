1b15a27889541002b5c191b54c2a3d125c1267ec
arrana16 <69128309+arrana16@users.noreply.github.com>
2023-09-27 16:41:38 -0400

    w

--- src/drivetrain/setup.cpp
+++ src/drivetrain/setup.cpp
@@ -16,4 +16,27 @@ lemlib::Drivetrain_t drivetrain {
     200
 };
 
-lemlib::Chassis chassis();
+lemlib::OdomSensors_t sensors {
+};
+lemlib::ChassisController_t lateralController {
+    8, // kP
+    30, // kD
+    1, // smallErrorRange
+    100, // smallErrorTimeout
+    3, // largeErrorRange
+    500, // largeErrorTimeout
+    5 // slew rate
+};
+ 
+// turning PID
+lemlib::ChassisController_t angularController {
+    4, // kP
+    40, // kD
+    1, // smallErrorRange
+    100, // smallErrorTimeout
+    3, // largeErrorRange
+    500, // largeErrorTimeout
+    0 // slew rate
+};
+
+lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
