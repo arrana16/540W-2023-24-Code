c2c512a7c89a07a7054131479d959a92378510bf
arrana16 <69128309+arrana16@users.noreply.github.com>
2023-10-28 17:07:11 -0400

    fsafsa

--- src/drivetrain/movement.cpp
+++ src/drivetrain/movement.cpp
@@ -1,9 +1,5 @@
 #include "main.h"
 
-//Great job gang
-//thanks gangy
-/* iiimmmmm*/
-//Great job
 void setVels(int linVel, int angVel) {
     int leftVel = linVel - (angVel*WHEELTRACK/2);
     int rightVel = linVel - (angVel*WHEELTRACK/2);
