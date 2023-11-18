c48d7b7845a159b92dbb48a0be3f1894ffe2e3b5
xikronz <carrie.chen71@gmail.com>
2023-11-17 22:33:34 -0500

    Update movement.cpp

--- src/drivetrain/movement.cpp
+++ src/drivetrain/movement.cpp
@@ -7,7 +7,9 @@
 void analyzeFile(const std::string& fileName){
     std::ifstream file(fileName);
     int pathTime = 0; 
-    file >> pathTime; 
+    file >> pathTime;
+
+    int clock = clock() 
 }
 
 void setVels(int linVel, int angVel) {
